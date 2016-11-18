de5cea424c8266d370e9282d9ff82b7278cc255c
Lars Bell <larsbell@gmail.com>
2016-11-17 20:44:56 -0500

    provides stemming service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { PorterstemmingService } from './services/porterstemming.service';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [
+    PorterstemmingService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
