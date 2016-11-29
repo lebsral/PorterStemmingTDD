72a578691f419ce978ab5d63c553f4cebea48625
Lars Bell <larsbell@gmail.com>
2016-11-28 20:38:10 -0500

    Executed 23531 of 23531 (5884 FAILED)

--- src/app/services/porterAllTests.service.spec.ts
+++ src/app/services/porterAllTests.service.spec.ts
@@ -5,7 +5,6 @@ import { PorterstemmingService } from './porterstemming.service';
 import { Input as input } from './input';
 import { Output as output } from './output';
 
-
 describe('Service: ALL', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,19 +12,13 @@ describe('Service: ALL', () => {
     });
   });
 
-//   let input = [
-//       'zodiacs', 'zounds'
-//       ];
-//   let output = [
-//       'zodiac', 'zound'
-//       ];
-
     for(let x = 0; x < input.length; x++) {
     test_my_times_ten(input[x], output[x]);
 };
 
   function test_my_times_ten(input, output) {
-     it('should return words that end in an ion  without the ion...', inject([PorterstemmingService], (service: PorterstemmingService) => {
+     it('should return all the words with their stem', inject([PorterstemmingService], (service: PorterstemmingService) => {
       expect(service.stemming(input)).toEqual(output);
  }));
-  });
+  }
+  );
